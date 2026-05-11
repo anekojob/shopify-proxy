d59e226e25560f2f8632082d88060fdadd02157f
anekojob <officialanekojob@gmail.com>
2026-05-11 10:58:24 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
 module.exports = async (req, res) => {
-  const shopifyDomain = "jobs24u.jobavasar.com";
+  const shopifyDomain = "talents.studysmarter.co.uk";
 
   // ✅ Domain → Ad config
   const domainAds = {
